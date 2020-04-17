c956ef0e891db8cbdef22488b67910164b96de6e
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-04-17 12:07:52 +0200

    add external models to scene

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import LevelDemo from "./level-demo.glb";
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
@@ -13,11 +15,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+
+var loader = new GLTFLoader();
+
+loader.load(
+  LevelDemo,
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 var animate = function () {
   requestAnimationFrame(animate);
