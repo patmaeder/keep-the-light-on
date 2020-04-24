afaeef183fd5422e00fcc931040b40f01996bcfa
Ali <46216799+althomi@users.noreply.github.com>
2020-04-24 21:15:44 +0200

    würfel mit three.js embedded + leuchtkraft würfel embedded

--- src/index.ts
+++ src/index.ts
@@ -77,10 +77,34 @@ const setupGraphics = async () => {
   dirLight.position.multiplyScalar(100);
   scene.add(dirLight);*/
 
+
   //pointlight
-  let pointLight = new THREE.PointLight(0xFFFFFF, 4, 2);
-  pointLight.position.set(1, 1, 1);
-  scene.add(pointLight);
+  let pointLight1 = new THREE.PointLight(0xFFFFF, 1, 2);
+  pointLight1.position.set(-1, 2, -1);
+  scene.add(pointLight1);
+
+  let pointLight2 = new THREE.PointLight(0xFFFFF, 1, 2);
+  pointLight2.position.set(-1, 2, 1);
+  scene.add(pointLight2);
+
+  let pointLight3 = new THREE.PointLight(0xFFFFF, 1, 2);
+  pointLight3.position.set(1, 1, 1);
+  scene.add(pointLight3);
+
+  //make cube with three.js
+ var geometry = new THREE.BoxGeometry(1,1,1); 
+ var cubeMaterial =[
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
+ ]
+
+ var cube1 = new THREE.Mesh(geometry, cubeMaterial)
+ scene.add(cube1)
+ cube1.position.set(-1,2,-1);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
