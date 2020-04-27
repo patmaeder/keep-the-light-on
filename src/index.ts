2622729d6c1e443d70bc7e3b8d342a0e8b663dad
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-04-27 14:05:23 +0200

    resolve merge fails by readding light and removing orbitcontrols

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ const setupGraphics = async () => {
   );
 
   camera.position.set(0, 4, 20);
-  
+
   let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.1);
   hemiLight.color.setHSL(0.6, 0.6, 0.6);
   hemiLight.groundColor.setHSL(0.1, 1, 0.4);
@@ -81,49 +81,44 @@ const setupGraphics = async () => {
   scene.add(hemiLight);
 
   //Add directional light
-  /*let dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  let dirLight = new THREE.DirectionalLight(0xffffff, 1);
   dirLight.color.setHSL(0.1, 1, 0.95);
   dirLight.position.set(-1, 1.75, 1);
   dirLight.position.multiplyScalar(100);
-  scene.add(dirLight);*/
-
+  scene.add(dirLight);
 
   //pointlight
-  let pointLight1 = new THREE.PointLight(0xFFFFF, 1, 2);
+  let pointLight1 = new THREE.PointLight(0xfffff, 1, 2);
   pointLight1.position.set(-1, 2, -1);
   scene.add(pointLight1);
 
-  let pointLight2 = new THREE.PointLight(0xFFFFF, 1, 2);
+  let pointLight2 = new THREE.PointLight(0xfffff, 1, 2);
   pointLight2.position.set(-1, 2, 1);
   scene.add(pointLight2);
 
-  let pointLight3 = new THREE.PointLight(0xFFFFF, 1, 2);
+  let pointLight3 = new THREE.PointLight(0xfffff, 1, 2);
   pointLight3.position.set(1, 1, 1);
   scene.add(pointLight3);
 
   //make cube with three.js
- var geometry = new THREE.BoxGeometry(1,1,1); 
- var cubeMaterial =[
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
-   new THREE.MeshPhongMaterial({color: 0xFFFFF, side: THREE.DoubleSide}), 
- ]
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var cubeMaterial = [
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+    new THREE.MeshPhongMaterial({ color: 0xfffff, side: THREE.DoubleSide }),
+  ];
 
- var cube1 = new THREE.Mesh(geometry, cubeMaterial)
- scene.add(cube1)
- cube1.position.set(-1,2,-1);
+  var cube1 = new THREE.Mesh(geometry, cubeMaterial);
+  scene.add(cube1);
+  cube1.position.set(-1, 2, -1);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  let controls = new OrbitControls(camera, renderer.domElement);
-  camera.position.set(20, 30, -10);
-  controls.update();
-  
   var loader = new GLTFLoader();
 
   //TODO Move to Cube.ts
@@ -139,7 +134,7 @@ const setupGraphics = async () => {
           cube = gltf.scene;
           //let geometry = new THREE.BoxGeometry( 1, 1, 1 );
           //let material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
-         // let cube = new THREE.Mesh(geometry, material);
+          // let cube = new THREE.Mesh(geometry, material);
           scene.add(cube);
           cube.position.set(3.5, 0.95, -6);
           cube.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
@@ -154,35 +149,34 @@ const setupGraphics = async () => {
     });
   await loadCube();
 
-
   //Load Tesmodul
   const loadTestmodul = () =>
-  new Promise((resolve, reject) => {
-    loader.load(
-      Testmodul,
-      function (gltfTestmodul) {
-        gltfTestmodul.scene.scale.x = 0.007;
-        gltfTestmodul.scene.scale.y = 0.007;
-        gltfTestmodul.scene.scale.z = 0.007;
-        testmodul = gltfTestmodul.scene;
-        scene.add(testmodul);
-        resolve();
-      },
-      undefined,
-      function (error) {
-        console.error(error);
-        reject();
-      }
-    );
-  });
+    new Promise((resolve, reject) => {
+      loader.load(
+        Testmodul,
+        function (gltfTestmodul) {
+          gltfTestmodul.scene.scale.x = 0.007;
+          gltfTestmodul.scene.scale.y = 0.007;
+          gltfTestmodul.scene.scale.z = 0.007;
+          testmodul = gltfTestmodul.scene;
+          scene.add(testmodul);
+          resolve();
+        },
+        undefined,
+        function (error) {
+          console.error(error);
+          reject();
+        }
+      );
+    });
   await loadTestmodul();
 };
 
-var scaleTestmodel = function(testmodel){
+var scaleTestmodel = function (testmodel) {
   testmodel.scene.scale.x = 0.25;
   testmodel.scene.scale.y = 0.25;
   testmodel.scene.scale.z = 0.25;
-}
+};
 
 const getPlayerMovement = () => {
   let left = inputHandler.isPressed([Key.ArrowLeft, "A"]);
@@ -200,14 +194,14 @@ const getPlayerMovement = () => {
 const animate = () => {
   const vector = getPlayerMovement();
   vector.multiplyScalar(0.13);
-  cube.rotateOnAxis(new THREE.Vector3(0,1,0), -(vector.x/4));
+  cube.rotateOnAxis(new THREE.Vector3(0, 1, 0), -(vector.x / 4));
   cube.translateY(vector.y);
   cube.translateZ(vector.z);
 
   let PivotPoint = new THREE.Object3D();
   cube.add(PivotPoint);
   PivotPoint.add(camera);
-  
+
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 };
