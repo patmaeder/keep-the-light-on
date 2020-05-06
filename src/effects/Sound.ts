008f3f3c9f1b878866977d7e9373b30dd1e78264
liviamxhk <lmaxhaku@gmail.com>
2020-05-06 15:42:18 +0200

    Bewegungs- & Einsammeln-Sound

--- src/effects/Sound.ts
+++ src/effects/Sound.ts
@@ -2,6 +2,7 @@ export default class Sound {
     init() {
         const audio = document.querySelector("audio");
         audio.volume = 0.2;
+        audio.loop = true;
         audio.play();
     }
 }
