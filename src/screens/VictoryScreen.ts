56b51cd5f96847372dd41221fa185ec0d3e6a404
Laurin <account@laurindoerre.de>
2020-05-14 01:38:37 +0200

    added stats

--- src/screens/VictoryScreen.ts
+++ src/screens/VictoryScreen.ts
@@ -1,7 +1,25 @@
+import {Screens} from "./Screens";
+import {toggleBackgroundMusic} from "../effects/Sound";
 
-
-/*
-super.addButton("Main Menu","backToMain", () => {
-    location.reload();
-});
-*/
+export class VictoryScreen extends Screens{
+    constructor(totalTime: number, totalLights: number, timeLeft: number) {
+        super("You Lost");
+        this.initButtons();
+        this.initSide(totalTime,totalLights,timeLeft);
+    }
+    private initButtons(){
+        super.addButton("restart","resume", () => {
+            super.switchVisibleStatus();
+            location.reload();
+        });
+        super.addButton("toggle sound","resume", () => {
+            toggleBackgroundMusic();
+        });
+        super.addButton("Main Menu","backToMain", () => {
+            location.reload();
+        });
+    }
+    private initSide(totalTime: number, totalLights: number, timeLeft: number){
+        super.setStatsBar(totalTime,totalLights,timeLeft);
+    }
+}
