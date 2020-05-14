9bc051559b45ad09c35693a15f488edf3af812ed
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2020-05-14 21:21:59 +0200

    fix typo

--- src/screens/introduction/introduction-page2.ts
+++ src/screens/introduction/introduction-page2.ts
@@ -11,9 +11,9 @@ export class IntroPage2 extends Introduction{
             `<img src="./game.svg" id="imgBig"></img>
             <p id="teaserText">Bringe den kleinen Würfel sicher ins Ziel bevor deine Zeit abgelaufen ist. Der Ablauf der Zeit wird in der Leiste am oberen 
                 Bildschirmrand visualisiert. <br> Um dir mehr Zeit auf dem Weg in die Freiheit zu verschaffen, sammle kleine, leuchtende Würfel, sogenannte Lichter, ein.
-                 Diese verschaffen die ein Plus an Zeit. Wie viele Lichter du bereits gesammelt hast, kannst du an der Anzeige in der oberen linken Ecke des Bildschirms ablesen.
+                 Diese verschaffen dir ein Plus an Zeit. Wie viele Lichter du bereits gesammelt hast, kannst du an der Anzeige in der oberen linken Ecke des Bildschirms ablesen.
                  <br>Nur wenn du das magisch leuchtende Portal vor Ablauf der Zeit erreichst, entfliehst du der Dunkelheit. 
-                  <br>Auf deinem Weg begegnest du zahlreichen Hindernissen und tiefe Schluchten, deren Überwindung kostbare Zeit erfordert.
+                  <br>Auf deinem Weg begegnest du zahlreichen Hindernissen und tiefen Schluchten, deren Überwindung kostbare Zeit erfordert.
             </p>
             <h2>Beeil dich!</h2>`;
     }
