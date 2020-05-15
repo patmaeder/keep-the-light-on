af90013ba043cba92b1a7aebca16482be8cd2345
Laurin <accoumt@laurindoerre.de>
2020-05-15 16:49:49 +0200

    cleanup

--- src/screens/BreakScreen.ts
+++ src/screens/BreakScreen.ts
@@ -2,24 +2,25 @@ import {Screens} from "./Screens";
 import {toggleMusic} from "../effects/Sound";
 import {introScreen1, introScreen2} from "../index";
 
-export class BreakScreen extends Screens{
+export class BreakScreen extends Screens {
     constructor() {
         super("Break");
         this.initButtons()
     }
-    private initButtons(){
-        super.addButton("resume","resume", () => {
+
+    private initButtons() {
+        super.addButton("resume", "resume", () => {
             super.switchVisibleStatus();
         });
-        super.addButton("toggle sound","resume", () => {
+        super.addButton("toggle sound", "resume", () => {
             toggleMusic();
         });
-        super.addButton("main menu","backToMain", () => {
+        super.addButton("main menu", "backToMain", () => {
             location.reload();
         });
-        super.addButton("Game Introduction","gameIntroduction", async () => {
+        super.addButton("Game Introduction", "gameIntroduction", async () => {
             super.switchVisibleStatus();
-            await new Promise((resolve,reject) => {
+            await new Promise((resolve, reject) => {
                 introScreen1.switchVisibleStatus();
                 let interval = setInterval(() => {
                     if (!introScreen1.isVisible() && !introScreen2.isVisible()) {
--- src/screens/LostScreen.ts
+++ src/screens/LostScreen.ts
@@ -1,25 +1,27 @@
 import {Screens} from "./Screens";
 import {toggleMusic} from "../effects/Sound";
 
-export class LostScreen extends Screens{
+export class LostScreen extends Screens {
     constructor(totalTime: number, totalLights: number, timeLeft: number) {
         super("You Lost");
         this.initButtons();
-        this.initSide(totalTime,totalLights,timeLeft);
+        this.initSide(totalTime, totalLights, timeLeft);
     }
-    private initButtons(){
-        super.addButton("restart","resume", () => {
+
+    private initButtons() {
+        super.addButton("restart", "resume", () => {
             super.switchVisibleStatus();
             location.reload();
         });
-        super.addButton("toggle sound","resume", () => {
+        super.addButton("toggle sound", "resume", () => {
             toggleMusic();
         });
-        super.addButton("Main Menu","backToMain", () => {
+        super.addButton("Main Menu", "backToMain", () => {
             location.reload();
         });
     }
-    private initSide(totalTime: number, totalLights: number, timeLeft: number){
-        super.setStatsBar(totalTime,totalLights,timeLeft);
+
+    private initSide(totalTime: number, totalLights: number, timeLeft: number) {
+        super.setStatsBar(totalTime, totalLights, timeLeft);
     }
 }
