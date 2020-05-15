a615b81e8ba3531c9a9ff99718c88c624a86031d
Laurin <accoumt@laurindoerre.de>
2020-05-15 16:49:33 +0200

    added background music

--- src/effects/Sound.ts
+++ src/effects/Sound.ts
@@ -1,39 +1,52 @@
-import { AudioLoader, Audio, AudioListener } from "three";
+import {Audio, AudioListener, AudioLoader} from "three";
 
 export default class Sound {
-  private listener = new AudioListener();
-  private audioLoader = new AudioLoader();
-  private sound: Audio;
-  constructor(camera, src: string) {
-    camera.add(this.listener);
-    this.sound = new Audio(this.listener);
-    this.initSound(src);
-  }
-  //Background musik  './assets/music/Melt-Down_Looping.mp3'
-  private initSound(src: string) {
-    console.log(src);
-    this.audioLoader.load(
-      src,
-      function (buffer) {
-        this.sound.setBuffer(buffer);
-        this.sound.setVolume(0.5);
-        this.sound.play();
-      }.bind(this)
-    );
-  }
-  public setPlaybackSpeed(rate: number){
-      this.sound.setPlaybackRate(rate);
-  }
-  public setLoop(loop: boolean){
+    private listener = new AudioListener();
+    private audioLoader = new AudioLoader();
+    private sound: Audio;
+
+    constructor(camera, src: string) {
+        camera.add(this.listener);
+        this.sound = new Audio(this.listener);
+        this.initSound(src);
+    }
+
+    public setPlaybackSpeed(rate: number) {
+        this.sound.setPlaybackRate(rate);
+    }
+
+    public setVolume(volume: number) {
+        this.sound.setVolume(volume);
+    }
+
+    public setLoop(loop: boolean) {
         this.sound.setLoop(loop);
     }
-    public play(){
+
+    public play() {
         this.sound.play();
     }
-    public pause(){
+
+    public pause() {
         this.sound.pause();
     }
+    public isPlaying(): boolean{
+        return this.sound.isPlaying;
+    }
+    //Background musik  './assets/music/Melt-Down_Looping.mp3'
+    private initSound(src: string) {
+        console.log(src);
+        this.audioLoader.load(
+            src,
+            function (buffer) {
+                this.sound.setBuffer(buffer);
+                this.sound.setVolume(0.5);
+                this.sound.play();
+            }.bind(this)
+        );
+    }
 }
+
 export function toggleMusic() {
-    this.sound.isPlaying ?  this.sound.play() :  this.sound.pause();
+    this.sound.isPlaying ? this.sound.play() : this.sound.pause();
 }
